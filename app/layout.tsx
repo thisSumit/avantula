4ef9006d2f5ce84b968a8927409c249a4b47081a
Sumit Karanjekar <122661393+thisSumit@users.noreply.github.com>
2024-06-21 23:53:36 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
    return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/icon.jpg" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
