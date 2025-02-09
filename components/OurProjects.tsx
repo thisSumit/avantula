8f1ce13179ae8ec792efd9da8d098fda3eb3f34d
Sumit Karanjekar <122661393+thisSumit@users.noreply.github.com>
2025-02-09 13:15:19 +0530

    Your commit message

--- components/OurProjects.tsx
+++ components/OurProjects.tsx
@@ -3,6 +3,7 @@ import { CircleArrowOutUpRightIcon } from 'lucide-react'
 import React from 'react'
 import { MdArrowOutward } from 'react-icons/md'
 import { Separator } from "@/components/ui/separator"
+import Image from 'next/image'
 
 export const OurProjects = () => {
   return (
@@ -14,7 +15,7 @@ export const OurProjects = () => {
             {projects.map(({id, title, des, img, link})=>(
                 <div key={id} className='lg:min-h-[30rem] h-fit flex items-start my-5'>
                     <div className='grid grid-cols-1 gap-1 relative md:grid-cols-2 '>
-                        <img src={img} alt={title} className='w-full relative h-fit rounded-xl'/>
+                        <Image width={400} height={200} src={img} alt={title} className='w-full relative h-fit rounded-xl'/>
                         <div className='flex flex-col relative font-bold text-[18px] mt-2 md:text-2xl md:mx-3 md:px-2'>
                         <h2 className='flex justify-start'>{title}</h2> 
                         <p className='font-light justify-center text-neutral-400 text-[14px] md:text-[18px]'>
